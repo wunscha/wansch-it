cfbd32353fd20d4c17a067401cc83b2567575553
wunscha <richhard.wansch@gmail.com>
2022-06-28 12:21:25 +0200

    hintergrund

--- hintergrund.js
+++ hintergrund.js
@@ -6,19 +6,16 @@ let optionenKnoten = {
     // Ebene 1
     1:  {
             radius: 0.5,
-            bewegungsradius: 100,
             geschwindigkeit: 0.3,
             farbe: 'rgb(200, 200, 200, 0.5)',
         },
     2: {
             radius: 1,
-            bewegungsradius: 200,
             geschwindigkeit: 0.7,
             farbe: 'rgb(200, 200, 200, 0.75)',
         },
     3:  {
             radius: 1.5,
-            bewegungsradius: 300,
             geschwindigkeit: 1.0,
             farbe: 'rgb(200, 200, 200, 1)',
         },
@@ -29,17 +26,34 @@ let contextHintergrund = canvasHintergrund.getContext('2d');
 let arrKnoten = [];
 let maxEntfernung = 50;
 
+let tid;
+window.addEventListener('resize', () => {
+    canvasHintergrund.width = window.innerWidth;
+    canvasHintergrund.height = window.innerHeight;
+    // ---
+    let summeVorher = 0;
+    arrKnoten.forEach(kn => {summeVorher += kn.richtungsvektor.y;});
+    console.log('Durchschnitt Vektor vorher:', summeVorher / arrKnoten.length);
+    // ---
+    initiiereHintergrund();
+    // ---
+    let summeNacher = 0;
+    arrKnoten.forEach(kn => {summeNacher += kn.richtungsvektor.y;});
+    console.log('Durchschnitt Vektor nachher:', summeNacher / arrKnoten.length);
+    // ---
+})
+
 function initiiereHintergrund() {
+    arrKnoten = [];
     for (let i=0; i < anzahlKnotenEbene; i++) {
-        arrKnoten.push(new Knoten(1));
+        arrKnoten.push(new Knoten(1, 'titel'));
     }
     for (let i=0; i < anzahlKnotenEbene; i++) {
-        arrKnoten.push(new Knoten(2));
+        arrKnoten.push(new Knoten(2, 'titel'));
     }
     for (let i=0; i < anzahlKnotenEbene; i++) {
-        arrKnoten.push(new Knoten(3));
+        arrKnoten.push(new Knoten(3, 'titel'));
     }
-    window.requestAnimationFrame(animationsschleife);
 }
 
 function animationsschleife() {
@@ -57,7 +71,7 @@ function animationsschleife() {
 }
 
 class Knoten {
-    constructor(ebene) {
+    constructor(ebene, idRandInnen) {
         // Optionen übernehmen
         Object.assign(this, optionenKnoten[ebene]);
         this.ebene = ebene;
@@ -65,26 +79,37 @@ class Knoten {
         this.y = this.yDefault = Math.random() * canvasHintergrund.height;
         this.richtungswinkel = Math.floor(Math.random() * 360);
         this.richtungsvektor = {
-            x: Math.cos(this.richtungswinkel) * this.geschwindigkeit,
+            // x: Math.cos(this.richtungswinkel) * this.geschwindigkeit,
+            x: 0,
             y: Math.sin(this.richtungswinkel) * this.geschwindigkeit,
+            // y: 0,
         }
-        this.inkrimentGeschwindigkeit = 0.1;
+        // Drehung um Längsachse
+        if (canvasHintergrund.width > canvasHintergrund.height) {
+            this.richtungsvektor.x = 0;
+            this.richtungsvektor.y = Math.sin(this.richtungswinkel) * this.geschwindigkeit;
+        } else {
+            this.richtungsvektor.x = Math.cos(this.richtungswinkel) * this.geschwindigkeit;
+            this.richtungsvektor.y = 0;
+        }
+        // Rand Innen
+        this.idRandInnen = idRandInnen;
+        
         // Funktionen
         this.aktualisiere = this.aktualisiere.bind(this);
-        this.beachteBewegungsradius = this.beachteBewegungsradius.bind(this);
+        this.beachteRandAußen = this.beachteRandAußen.bind(this);
+        this.beachteRandInnen = this.beachteRandInnen.bind(this);
         this.zeichneKnoten = this.zeichneKnoten.bind(this);
     }
     
     aktualisiere() {
-        this.beachteBewegungsradius();
-        if (this.geschwindigkeit == 100) {this.inkrementGeschwindigkeit = -0.1}
-        if (this.geschwindigkeit == 0.1) {this.inkrementGeschwindigkeit = 0.1;}
-        this.geschwindigkeit += this.inkrementGeschwindigkeit;
-        this.x += this.richtungsvektor.x;
-        this.y += this.richtungsvektor.y;
+        this.beachteRandAußen();
+        this.beachteRandInnen();
+        this.x += this.richtungsvektor.x * this.beschleunigungsFaktor;
+        this.y += this.richtungsvektor.y * this.beschleunigungsFaktor;
     }
 
-    beachteBewegungsradius() {
+    beachteRandAußen() {
         // Bande X-Richtung
         if (this.x >= canvasHintergrund.width || this.x <= 0) {
             this.richtungsvektor.x *= -1;
@@ -92,6 +117,53 @@ class Knoten {
         // Bande Y-Richtung
         if (this.y > canvasHintergrund.height || this.y <= 0) {
             this.richtungsvektor.y *= -1;
+        }
+    }
+
+    beachteRandInnen() {
+        let domRectRandInnen = document.querySelector(`#${this.idRandInnen}`).getBoundingClientRect();
+        this.sperrbereich = {
+            unten: canvasHintergrund.height - domRectRandInnen.bottom,
+            oben: canvasHintergrund.height - domRectRandInnen.top,
+            links: domRectRandInnen.left,
+            rechts: domRectRandInnen.right,
+        }
+        // Ist Punkt im Sperrbereich?
+        let knotenIstImSperrbereich = (
+            this.y > this.sperrbereich.unten &&
+            this.y < this.sperrbereich.oben &&
+            this.x > this.sperrbereich.links &&
+            this.x < this.sperrbereich.rechts
+        );
+        if (knotenIstImSperrbereich) {
+            // Nächsten Rand suchen
+            let abstandRand = Math.abs(this.y - this.sperrbereich.unten);
+            let naechsterRand = 'unten';
+            if (Math.abs(this.y - this.sperrbereich.oben) < abstandRand) {
+                abstandRand = Math.abs(this.y - this.sperrbereich.oben);
+                naechsterRand = 'oben';
+            };
+            if (Math.abs(this.x - this.sperrbereich.links) < abstandRand) {
+                abstandRand = Math.abs(this.x - this.sperrbereich.links);
+                naechsterRand = 'links';
+            };
+            if (Math.abs(this.x - this.sperrbereich.rechts) < abstandRand) {
+                abstandRand = Math.abs(this.x - this.sperrbereich.links);
+                naechsterRand = 'rechts';
+            };
+            // Richtung umkehren
+            if (naechsterRand == 'unten' || naechsterRand == 'oben') {
+                this.richtungsvektor.y *= -1;
+                this.y = this.sperrbereich[naechsterRand];
+            }
+            if (naechsterRand == 'links' || naechsterRand == 'rechts') {
+                this.richtungsvektor.x *= -1;
+                this.x = this.sperrbereich[naechsterRand];
+            }
+            // Beschleunigen
+            this.beschleunigungsFaktor = 2;
+        } else {
+            this.beschleunigungsFaktor = 1;
         }
     }
 
@@ -122,5 +194,6 @@ function anpasseGrösse() {
 // TEST
 anpasseGrösse();
 initiiereHintergrund();
+window.requestAnimationFrame(animationsschleife);
 // let knoten = new Knoten(1);
 // knoten.zeichne();
